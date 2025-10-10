8def702875b29520454e3f12a82ccc17849d6082
olivsinz <zinsouoliviers@gmail.com>
2025-10-10 16:29:32 +0000

    WIP

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+
 // use RectorLaravel\Set\LaravelSetList;
 // use RectorLaravel\Set\LaravelSetProvider;
 
@@ -36,25 +37,25 @@ return RectorConfig::configure()
         earlyReturn: true,
         strictBooleans: true,
     );
-    // ->withSetProviders(LaravelSetProvider::class)
-    // ->withComposerBased(laravel: true)
-    // ->withSets([
-    //     LaravelSetList::LARAVEL_CODE_QUALITY,
-    //     LaravelSetList::LARAVEL_COLLECTION,
-    //     LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
-    //     LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
-    //     LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
-    //     LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
-    //     LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
-    //     LaravelSetList::LARAVEL_FACTORIES,
-    //     LaravelSetList::LARAVEL_IF_HELPERS,
-    //     LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
-    //     LaravelSetList::LARAVEL_STATIC_TO_INJECTION,
-    //     LaravelSetList::LARAVEL_TESTING,
-    //     LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
-    // ])
-    // ->withConfiguredRule(RemoveDumpDataDeadCodeRector::class, [
-    //     'dd', 'dump', 'var_dump',
-    // ])
-    // ->withConfiguredRule(RouteActionCallableRector::class)
-    // ->withConfiguredrule(WhereToWhereLikeRector::class);
+// ->withSetProviders(LaravelSetProvider::class)
+// ->withComposerBased(laravel: true)
+// ->withSets([
+//     LaravelSetList::LARAVEL_CODE_QUALITY,
+//     LaravelSetList::LARAVEL_COLLECTION,
+//     LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+//     LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+//     LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+//     LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+//     LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+//     LaravelSetList::LARAVEL_FACTORIES,
+//     LaravelSetList::LARAVEL_IF_HELPERS,
+//     LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+//     LaravelSetList::LARAVEL_STATIC_TO_INJECTION,
+//     LaravelSetList::LARAVEL_TESTING,
+//     LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
+// ])
+// ->withConfiguredRule(RemoveDumpDataDeadCodeRector::class, [
+//     'dd', 'dump', 'var_dump',
+// ])
+// ->withConfiguredRule(RouteActionCallableRector::class)
+// ->withConfiguredrule(WhereToWhereLikeRector::class);
